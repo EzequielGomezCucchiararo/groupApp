f70db787b852d2a4fd6ca1ce8f028690b8cb578b
EzequielGomezCucchiararo <ezequiel.cucchiararo@gmail.com>
2017-02-07 12:46:18 +0100

    calling for categories with ng-click resolved

--- app/home/categorySection/category.controller.js
+++ app/home/categorySection/category.controller.js
@@ -4,15 +4,48 @@
 	    .module('app')
 	    .controller('CategoryHomeController', CategoryHomeController)
 
-  function CategoryHomeController () {
+  function CategoryHomeController (categoryFactory) {
   	var vm = this
 
-  	this.categoriesOne = ['adventure', 'action', 'family', 'horror']
+  	this.categoriesOne = [
+  		{
+  			name: 'adventure',
+  			id: 12
+  		},
+  		{
+  			name: 'action',
+  			id: 28
+  		},
+  		{
+  			name: 'family',
+  			id: 10751
+  		},
+  		{
+  			name: 'horror',
+  			id: 27
+  		}
+  	]
 
-  	this.categoriesTwo = ['animation', 'comedy', 'documentary', 'romance']
+  	this.categoriesTwo = [
+  		{
+  			name: 'animation',
+  			id: 16
+  		},
+  		{
+  			name: 'comedy',
+  			id: 35
+  		},
+  		{
+  			name: 'documentary',
+  			id: 99
+  		},
+  		{
+  			name: 'romance',
+  			id: 10749
+  		}]
 
-  	this.openCategory = function openCategory () {
-
+  	this.openCategory = function openCategory (categoryID) {
+  		console.log(categoryFactory.getList(categoryID))
   	}
   }
 })()
--- app/home/categorySection/category.factory.js
+++ app/home/categorySection/category.factory.js
@@ -1,10 +1,18 @@
 (function () {
   angular
     .module('app')
-    .factory('getCategory', getCategory)
+    .factory('categoryFactory', categoryFactory)
 
-  function getCategory ($http) {
-  	var url
+  function categoryFactory ($http) {
+  	var apiKey = '0ea733e6e9ff0120779fcfb8e7b9439d'
+
+  	function getList (categoryID) {
+  		return $http.get('https://api.themoviedb.org/3/genre/' + categoryID + '/movies?api_key=' + apiKey + '&language=en-US&include_adult=false&sort_by=created_at.asc')
+  	}
+
+  	return {
+  		getList: getList
+  	}
   }
 })()
 
