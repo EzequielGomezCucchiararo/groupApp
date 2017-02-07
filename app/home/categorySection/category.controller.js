44255e486f999d6e9357d2b442b15202b132a0d0
bijay timilsina <bjtimi.007@gmail.com>
2017-02-07 13:05:52 +0100

    added vm and edits category2

--- app/home/categorySection/category.controller.js
+++ app/home/categorySection/category.controller.js
@@ -1,52 +1,52 @@
 
 (function () {
   angular
-	    .module('app')
-	    .controller('CategoryHomeController', CategoryHomeController)
+      .module('app')
+      .controller('CategoryHomeController', CategoryHomeController)
 
   function CategoryHomeController (categoryFactory) {
-  	var vm = this
+    var vm = this
 
-  	this.categoriesOne = [
-  		{
-  			name: 'adventure',
-  			id: 12
-  		},
-  		{
-  			name: 'action',
-  			id: 28
-  		},
-  		{
-  			name: 'family',
-  			id: 10751
-  		},
-  		{
-  			name: 'horror',
-  			id: 27
-  		}
-  	]
+    vm.categoriesOne = [
+      {
+        name: 'adventure',
+        id: 12
+      },
+      {
+        name: 'action',
+        id: 28
+      },
+      {
+        name: 'family',
+        id: 10751
+      },
+      {
+        name: 'horror',
+        id: 27
+      }
+    ]
 
-  	this.categoriesTwo = [
-  		{
-  			name: 'animation',
-  			id: 16
-  		},
-  		{
-  			name: 'comedy',
-  			id: 35
-  		},
-  		{
-  			name: 'documentary',
-  			id: 99
-  		},
-  		{
-  			name: 'romance',
-  			id: 10749
-  		}]
+    vm.categoriesTwo = [
+      {
+        name: 'animation',
+        id: 16
+      },
+      {
+        name: 'comedy',
+        id: 35
+      },
+      {
+        name: 'documentary',
+        id: 99
+      },
+      {
+        name: 'romance',
+        id: 10749
+      }]
 
-  	this.openCategory = function openCategory (categoryID) {
-  		console.log(categoryFactory.getList(categoryID))
-  	}
+    vm.openCategory = function openCategory (categoryID) {
+      console.log(categoryFactory.getList(categoryID))
+    }
   }
 })()
 
